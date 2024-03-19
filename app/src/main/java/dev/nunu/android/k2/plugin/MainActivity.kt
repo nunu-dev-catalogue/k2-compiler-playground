e5951588bb71c017457334af3611a62aa653efa5
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-03-19 10:29:55 +0900

    Change theme of root screen

--- app/src/main/java/dev/nunu/android/k2/plugin/MainActivity.kt
+++ app/src/main/java/dev/nunu/android/k2/plugin/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.nunu.android.k2.plugin.ui.theme.K2CompilerPluginPracticeTheme
+import dev.nunu.android.k2.plugin.ui.theme.Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            K2CompilerPluginPracticeTheme {
+            Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    K2CompilerPluginPracticeTheme {
+    Theme {
         Greeting("Android")
     }
 }
